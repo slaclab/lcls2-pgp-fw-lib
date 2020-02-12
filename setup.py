d094ad6f5d448ba243054911a83f5a89a302b188
Ryan Herbst <rherbst@slac.stanford.edu>
2020-02-11 23:45:11 -0800

    Add anaconda recipe

---
+++ setup.py
@@ -0,0 +1,24 @@
+
+from distutils.core import setup
+from git import Repo
+
+repo = Repo()
+
+# Get version before adding version file
+ver = repo.git.describe('--tags')
+
+# append version constant to package init
+with open('python/lcls2_pgp_fw_lib/__init__.py','a') as vf:
+    vf.write(f'\n__version__="{ver}"\n')
+
+setup (
+   name='lcls2_pgp_fw_lib',
+   version=ver,
+   packages=['lcls2_pgp_fw_lib', 
+             'lcls2_pgp_fw_lib/hardware/', 
+             'lcls2_pgp_fw_lib/hardware/SlacPgpCardG4', 
+             'lcls2_pgp_fw_lib/hardware/XilinxKcu1500', 
+             'lcls2_pgp_fw_lib/hardware/shared',],
+   package_dir={'':'python'},
+)
+
