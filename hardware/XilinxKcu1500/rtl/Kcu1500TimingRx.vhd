719f2581e3f0b7673b9338aabbdab8c9daa895b5
Matt Weaver <weaver@slac.stanford.edu>
2020-05-29 21:53:41 -0700

    Set default for new input signal

--- hardware/XilinxKcu1500/rtl/Kcu1500TimingRx.vhd
+++ hardware/XilinxKcu1500/rtl/Kcu1500TimingRx.vhd
@@ -59,7 +59,7 @@ entity Kcu1500TimingRx is
       eventRst                 : in  sl;
       eventTimingMessagesValid : out slv(NUM_DETECTORS_G-1 downto 0);
       eventTimingMessages      : out TimingMessageArray(NUM_DETECTORS_G-1 downto 0);
-      eventTimingMessagesRd    : in  slv(NUM_DETECTORS_G-1 downto 0);
+      eventTimingMessagesRd    : in  slv(NUM_DETECTORS_G-1 downto 0) := (others=>'1');
       eventAxisMasters         : out AxiStreamMasterArray(NUM_DETECTORS_G-1 downto 0);
       eventAxisSlaves          : in  AxiStreamSlaveArray(NUM_DETECTORS_G-1 downto 0);
       eventAxisCtrl            : in  AxiStreamCtrlArray(NUM_DETECTORS_G-1 downto 0);
