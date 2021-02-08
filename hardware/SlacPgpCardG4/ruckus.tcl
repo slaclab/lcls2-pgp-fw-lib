40d2075f840106dc8081aac4dbf5c2491dd75fec
Larry Ruckman <ruckman@slac.stanford.edu>
2021-02-08 13:14:25 -0800

    switching to PGP4 link rate to 6.25Gb/s because of layout issues on FEB board

--- hardware/SlacPgpCardG4/ruckus.tcl
+++ hardware/SlacPgpCardG4/ruckus.tcl
@@ -6,7 +6,7 @@ loadSource      -lib lcls2_pgp_fw_lib -dir  "$::DIR_PATH/rtl"
 loadConstraints -path "$::DIR_PATH/xdc/SlacPgpCardG4Hsio.xdc"
 
 # Case the timing on communication protocol
-if { [info exists ::env(INCLUDE_PGP4_10G)] != 1 || $::env(INCLUDE_PGP4_10G) == 0 } {
+if { [info exists ::env(INCLUDE_PGP4_6G)] != 1 || $::env(INCLUDE_PGP4_6G) == 0 } {
    loadConstraints -path "$::DIR_PATH/xdc/Pgp2bTiming.xdc"
 } else {
    loadConstraints -path "$::DIR_PATH/xdc/Pgp4Timing.xdc"
