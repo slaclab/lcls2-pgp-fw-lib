7794693ba30123934c63593f5e28caed4686794d
Larry Ruckman <ruckman@slac.stanford.edu>
2021-08-26 10:28:36 -0700

    updating lcls-timing-core be >= v3.6.0

--- shared/ruckus.tcl
+++ shared/ruckus.tcl
@@ -10,7 +10,7 @@ if { [VersionCheck 2018.2 ] < 0 } {
 if { [info exists ::env(OVERRIDE_SUBMODULE_LOCKS)] != 1 || $::env(OVERRIDE_SUBMODULE_LOCKS) == 0 } {
    if { [SubmoduleCheck {axi-pcie-core}    {3.5.3}  ] < 0 } {exit -1}
    if { [SubmoduleCheck {l2si-core}        {3.3.2}  ] < 0 } {exit -1}
-   if { [SubmoduleCheck {lcls-timing-core} {3.2.4}  ] < 0 } {exit -1}
+   if { [SubmoduleCheck {lcls-timing-core} {3.6.0}  ] < 0 } {exit -1}
    if { [SubmoduleCheck {ruckus}           {2.9.2}  ] < 0 } {exit -1}
    if { [SubmoduleCheck {surf}             {2.13.0} ] < 0 } {exit -1}
 } else {
